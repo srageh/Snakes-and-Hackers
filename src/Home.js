52b78d1b58eae6c4a3d76206fd52d3f6e55b4b62
srageh <ragehsuhaib@gmail.com>
2021-01-09 21:03:23 -0500

    fixed house screen navigation

--- src/Home.js
+++ src/Home.js
@@ -5,23 +5,33 @@ import { Link } from "react-router-dom";
 
 
 const Home = ()=> {
+  
+  function gameType(value){
+    document.getElementById('playButton').style.display = 'flex';
+
+  }
+
+  function playGame(){
+
+  }
+
   return (
     <div className="choose-game-cotainer">
         <h2 style={{textAlign:"center", margin:"2em 0", fontSize:"3em"}}>Choose Game Mode</h2>
         <div className="flex-2">
-        <div className="solo-game">
+        <div onClick={()=>gameType(1)} className="solo-game">
             <h2>Solo Game</h2>
             <p>play against computer</p>
         </div>
-        <div className="multipayer-game">
+        <div onClick={()=>gameType(2)} className="multipayer-game">
             <h2>Multiplayer Game</h2>
             <p>play against friends</p>
         </div>
  
         
         </div>
-        <div style={{display:"flex", justifyContent:"center"}}>
-          <Link className="play-button"  to="/house" >Play</Link>
+        <div id="playButton"  style={{display:"none", justifyContent:"center"}}>
+          <Link onClick={()=> playGame()} className="play-button"  to="/house" >Play</Link>
        
         </div>
         
--- src/House.js
+++ src/House.js
@@ -1,27 +1,65 @@
-import React from "react";
+
 import './App.css';
 import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
 
 const House = ()=> {
+  var x = 0;
+  const [house, setHouse] = useState({imgage:'', name:''});
+ 
+  function startGame(){
+
+   
+  
+
+
+  }
+
+  useEffect(()=>{
+
+  })
+
+  function houseValue(value){
+  
+    document.getElementById('btn').style.display = "flex";
+
+    //bnt.style.display = 'flex';
+
+    if(value == "Hufflepuff"){
+      fetch('http://hp-api.herokuapp.com/api/characters/staff/')
+      .then(resp=>resp.json())
+      .then(data=> console.log(data))
+    }
+    else{
+      fetch('http://hp-api.herokuapp.com/api/characters/house/' + value)
+      .then(resp=> resp.json())
+      .then(house=> { setHouse({image:house.name, name:house.name }); console.log(house)})
+
+    }
+
+   
+  }
+
+
   return (
     <div className="choose-game-cotainer">
         <h2 style={{textAlign:"center", margin:"1em 0", fontSize:"3em"}}>Choose House</h2>
         <div className="flex-2">
 
-          <div className="solo-game">
+          <div onClick={ ()=>houseValue("Ravenclaw")} className="solo-game">
               <h2>Ravenclaw</h2>
               <img  src={"raven.png"}/>
               <p>cath phrase</p>
           </div>
           
-          <div className="solo-game">
+          <div onClick={()=>houseValue("Gryffindor")} className="solo-game">
 
               <h2>Gryffindor</h2>
               <img  src={"gryf.png"}/>
               <p>cath phrase</p>
           </div>
 
-          <div className="solo-game">
+          <div onClick={()=>houseValue("Hufflepuff")} className="solo-game">
               <h2>Hufflepuff</h2>
               <img  src={"huffle.png"}/>
               <p>cath phrase</p>
@@ -30,7 +68,7 @@ const House = ()=> {
 
          
 
-          <div className="solo-game">
+          <div onClick={()=>houseValue("Slytherin")} className="solo-game">
               <h2>Slytherin</h2>
               <img  src={"slytg.png"}/>
               <p>cath phrase</p>
@@ -41,8 +79,9 @@ const House = ()=> {
         
         </div>
         
-        <div style={{display:"flex", justifyContent:"center"}}>
-          <Link className="play-button"  to="/game" >Start Game</Link>
+        <div id="btn" style={{display:"none", justifyContent:"center"}}>
+         
+          <Link className="play-button" onClick={startGame} house={house}   to="/game" >Start Game</Link>
        
         </div>
       
